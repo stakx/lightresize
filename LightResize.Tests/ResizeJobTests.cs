0dbd80461851b7d066e0f55db1e8363633f6d3f2
stakx <stakx@eml.cc>
2017-05-14 00:25:06 +0200

    Remove dependency on `System.Web` in unit tests
    The description of LightResize states that it is to be used in non-
    ASP.NET context. For this reason, it appears somewhat inconsistent
    to have a unit test for it that parses query strings.
    
    This commit changes the unit test in question to no longer depend on
    a query string. Then the dependency on `System.Web` can be removed.

--- LightResize.Tests/ResizeJobTests.cs
+++ LightResize.Tests/ResizeJobTests.cs
@@ -235,15 +235,20 @@ namespace LightResize.Tests
         }
 
         [Test]
-        [TestCase(50, 50, "format=jpg&quality=100")]
-        [TestCase(1, 1, "format=jpg&quality=100")]
-        [TestCase(50, 50, "format=jpg&quality=-300")]
-        [TestCase(50, 50, "format=png")]
-        public void EncodeImage(int width, int height, string query)
+        [TestCase(50, 50, OutputFormat.Jpg, 100)]
+        [TestCase(1, 1, OutputFormat.Jpg, 100)]
+        [TestCase(50, 50, OutputFormat.Jpg, -300)]
+        [TestCase(50, 50, OutputFormat.Png)]
+        public void EncodeImage(int width, int height, OutputFormat format, int? jpegQuality = null)
         {
             using (MemoryStream ms = new MemoryStream(8000))
             {
-                Job(query).Build(GetBitmapStream(width, height), ms, JobOptions.LeaveTargetStreamOpen);
+                var job = new ResizeJob(format: format, jpegQuality: jpegQuality ?? 90)
+                {
+                    Format = format,
+                    JpegQuality = jpegQuality ?? 90,
+                };
+                job.Build(GetBitmapStream(width, height), ms, JobOptions.LeaveTargetStreamOpen);
             }
         }
 
@@ -269,61 +274,6 @@ namespace LightResize.Tests
 
             ms.Seek(0, SeekOrigin.Begin);
             return ms;
-        }
-
-        // Allows an ResizeJob to be configured from a query string-style string.
-        // Supports `width`, `height`, `quality`, `scale`, `mode`, `format`, `ignoreicc`, but not `matte`.
-        private ResizeJob Job(string str)
-        {
-            var nvc = HttpUtility.ParseQueryString(str);
-
-            var j = new ResizeJob();
-            j.Width = ParseInt(nvc, "width", j.Width);
-            j.Height = ParseInt(nvc, "height", j.Height);
-            j.JpegQuality = ParseInt(nvc, "quality", j.JpegQuality).Value;
-            if ("true".Equals(nvc["ignoreicc"], StringComparison.OrdinalIgnoreCase))
-            {
-                j.IgnoreIccProfile = true;
-            }
-
-            j.ScalingRules = ParseEnum<ScaleMode>(nvc, "scale", j.ScalingRules).Value;
-            j.Mode = ParseEnum<FitMode>(nvc, "mode", j.Mode).Value;
-            j.Format = ParseEnum<OutputFormat>(nvc, "format", j.Format).Value;
-
-            /* Didn't parse color, too hard */
-
-            return j;
-        }
-
-        private T? ParseEnum<T>(NameValueCollection nvc, string key, T? def)
-            where T : struct, IConvertible
-        {
-            string s = nvc[key];
-            if (!string.IsNullOrEmpty(s))
-            {
-                T temp;
-                if (Enum.TryParse<T>(s, out temp))
-                {
-                    return temp;
-                }
-            }
-
-            return def;
-        }
-
-        private int? ParseInt(NameValueCollection nvc, string key, int? def)
-        {
-            string s = nvc[key];
-            if (!string.IsNullOrEmpty(s))
-            {
-                int temp;
-                if (int.TryParse(s, out temp))
-                {
-                    return temp;
-                }
-            }
-
-            return def;
         }
     }
 }
